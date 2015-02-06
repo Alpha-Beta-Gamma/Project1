3c48f4f3ae274d09aa033515b9844901f8cea943
gbsuccar <georgesuccar11@yahoo.com>
2015-02-06 08:48:50 -0800

    Added "Test Code" folder with one file "Course.java"
    I have added a "Test Code" folder to the project folder to act as a
    sandbox to run code.  In this currently is the "Course.java" file, a
    test of the course class that will keep track of class grade
    distributions and information.

---
+++ Test Code/Course.java
@@ -0,0 +1,48 @@
+
+package course;
+
+import java.util.*;
+
+public class Course {
+    String name;
+    int number;
+    String professor;
+    HashMap<String, Integer> distribution;
+    
+    public Course(String name, int number, String professor){
+        this.name = name;
+        this.number = number;
+        this.professor = professor;
+        distribution = new HashMap<String, Integer>();
+    }
+    
+    //getter methods
+    public String getName(){
+        return name;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public String getProfessor(){
+        return professor;
+    }
+    
+    //setter methods if edits are needed (shouldn't be needed, just in case)
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+    public void setProfessor(String professor){
+        this.professor = professor;
+    }
+    
+    //test
+    distribution.put("Test", 1);
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
