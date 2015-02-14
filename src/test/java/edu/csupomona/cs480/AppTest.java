a5e571c13912d24aad683a44934e3a1065775a08
Preston Lomenzo <prestonlomenzo@hotmail.com>
2015-02-13 18:57:14 -0800

    Unit Test for assignment 6

--- src/test/java/edu/csupomona/cs480/AppTest.java
+++ src/test/java/edu/csupomona/cs480/AppTest.java
@@ -1,5 +1,7 @@
 package edu.csupomona.cs480;
 
+import java.util.HashMap;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +9,36 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+   /**
+    * Create the test case
+    *
+    * @param testName name of the test case
+    */
+   public AppTest(String testName) {
+      super(testName);
+   }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+   /**
+    * @return the suite of tests being tested
+    */
+   public static Test suite() {
+      return new TestSuite(AppTest.class);
+   }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+   /**
+    * Rigourous Test :-)
+    */
+   public void testApp() {
+      assertTrue(true);
+   }
+
+   public void testMissingValue() {
+      Course c = new Course("Databases", 48043, "Smith", new HashMap<String, Integer>());
+      double[] scores = { 30.0, 20.0, 10.0, 5.0 };
+      double outOf = 100.0;
+
+      assertEquals(c.calculateMissingValue(scores, outOf), 35.0, 0.0);
+
+   }
 }
--- src/test/java/edu/csupomona/cs480/Course.java
+++ src/test/java/edu/csupomona/cs480/Course.java
@@ -1,6 +1,7 @@
 package edu.csupomona.cs480;
 
 import java.util.*;
+
 //import java.lang.Exception;//might be needed
 
 public class Course {
@@ -9,8 +10,7 @@ public class Course {
    String professor;
    static HashMap<String, Integer> distribution;
 
-   public Course(String name, int number, String professor,
-      HashMap<String, Integer> dist) {
+   public Course(String name, int number, String professor, HashMap<String, Integer> dist) {
       this.name = name;
       this.number = number;
       this.professor = professor;
@@ -42,32 +42,50 @@ public class Course {
    public void setProfessor(String professor) {
       this.professor = professor;
    }
+
    /*
     Returns the current distribution -- to change grade distribution,
       use getDistribution(), change grade distribution, then call
       changeDistribution(newDistribution);
       !!Make sure total percentages add to up 100!!
    */
-   public HashMap<String, Integer> getDistribution(){
-	   return distribution;
+   public HashMap<String, Integer> getDistribution() {
+      return distribution;
    }
+
    /*
     Changes the old grade distribution to a given grade distribution
     */
-   public void changeDistribution(HashMap<String, Integer> changedDistribution){
-       distribution = changedDistribution;
+   public void changeDistribution(HashMap<String, Integer> changedDistribution) {
+      distribution = changedDistribution;
    }
+
    /*
     Probably won't use checkGoodPercent, input verification to be
       handled elsewhere.
     */
-   public boolean checkGoodPercent(HashMap<String, Integer> test){
-        int totalPercent = 0;
-        for(int value : test.values()){
-                totalPercent += value;
-        }
-        return (totalPercent == 100);
+   public boolean checkGoodPercent(HashMap<String, Integer> test) {
+      int totalPercent = 0;
+      for (int value : test.values()) {
+         totalPercent += value;
+      }
+      return (totalPercent == 100);
    }
+
+   /*
+    * this takes in a list of values of scores recieved by the student
+    * and caluclates much more percentage the student needs inorder to reach outOf
+    * outOF will tipcally be 100 for percentage pased classes, or it will be how ever many points
+    * the class is based on
+    */
+   public double calculateMissingValue(double[] scores, double outOf) {
+      double total = 0;
+      for (double cur : scores)
+         total += cur;
+
+      return outOf - total;
+   }
+
    /*
     returns the characteristics of a class in the form:
       <Class Name> | <Class Number> | <Professor> |
@@ -75,12 +93,9 @@ public class Course {
       name, number, professor and distribution set 
       are delimited by the "|" vertical bar symbol
     */
-   public String toString(){
-	   String strungClass = new String(name + " | " + number + " | "
-                   + professor + " | " + "\n" + distribution.toString());
-           return strungClass;
+   public String toString() {
+      String strungClass = new String(name + " | " + number + " | " + professor + " | " + "\n" + distribution.toString());
+      return strungClass;
    }
-   
-   
-}
 
+}
