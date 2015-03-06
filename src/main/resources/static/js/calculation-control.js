4ed6dc7ed4e89d71ed852fe45b3174f6cb73c6ee
Preston Lomenzo <prestonlomenzo@hotmail.com>
2015-03-05 19:19:35 -0800

    You can now calculate your grade

--- src/main/resources/static/js/calculation-control.js
+++ src/main/resources/static/js/calculation-control.js
@@ -1,5 +1,6 @@
 
 var classId = ""; //use this to find out what data to load for this page
+var totalAtt = 0;
 
 function goCalcPage(id) {
 	window.location.href = "/calculationpage.html?id=" + id;
@@ -56,7 +57,7 @@ function addData(){
 	} else {
 		var newTable = document.createElement("TABLE");
 		var table = "<tr><td></td><td>Name     </td><td>Value    </td><td>Your Score</td></tr>";
-		var attCount = 0;
+		totalAtt = 0;
 		
 		$.ajax(
 				{
@@ -65,14 +66,14 @@ function addData(){
 					data : {
 					},
 					success : function(result) {
-						attCount = result;
+						totalAtt = result;
 					},async:false,
 					error: function (jqXHR, exception) {
 						alert("Failed to get class attributes.");
 					}
 				});
 		
-		for (var i = 0; i < attCount; i++){
+		for (var i = 0; i < totalAtt; i++){
 			$.ajax(
 					{
 						type : "GET",
@@ -82,13 +83,15 @@ function addData(){
 							"index" : i
 						},
 						success : function(result) {
-							table += '<tr><td></td><td>' + result.name + '</td><td>' + result.value + '</td><td><input id="score' + i + '" onKeyPress="return numbersonly(this, event)" value=""/></td></tr>';
+							table += '<tr><td></td><td>' + result.name + '</td><td>' + result.value + '</td><td><input id="score' + i + '" onKeyPress="return numbersonly(this, event)" placeholder="Your score" value=""/></td></tr>';
 						},async:false,
 						error: function (jqXHR, exception) {
 							alert("Failed to get class attributes. Error 2.");
 						}
 				});
 		}
+		
+		table += '<tr><td></td><td>Letter Grade Wanted</td><td></td><td><input id="gradewanted" placeholder="Letter grade you want" value=""/></td></tr>';
 
 		newTable.innerHTML = table;
 		document.getElementById('dynamicInput').appendChild(newTable);
@@ -98,7 +101,63 @@ function addData(){
 
 function calculateGrade()
 {
-	alert(classId);
-	//TODO will have to use loop to get each id="score + INDEX"
-	//can use example from loading text into combo boxs
+	//goes through all input boxes and gets the total
+	var totalGot = 0;
+	
+	for (var i = 0; i < totalAtt; i++){
+		totalGot += parseInt($('#score' + i).val());
+	}
+	
+	var wantedGrade = $('#gradewanted').val();
+	var output = "";
+	var missing = 0;
+	
+	if (wantedGrade == "A+"){
+		var min = 97;
+		missing = min - totalGot; 
+	}else if (wantedGrade == "A"){
+		var min = 93;
+		missing = min - totalGot;
+	}else if (wantedGrade == "A-"){
+		var min = 90;
+		missing = min - totalGot;
+	}else if (wantedGrade == "B+"){
+		var min = 87;
+		missing = min - totalGot;
+	}else if (wantedGrade == "B"){
+		var min = 83;
+		missing = min - totalGot;
+	}else if (wantedGrade == "B-"){
+		var min = 80;
+		missing = min - totalGot;
+	}else if (wantedGrade == "C+"){
+		var min = 77;
+		missing = min - totalGot;
+	}else if (wantedGrade == "C"){
+		var min = 73;
+		missing = min - totalGot;
+	}else if (wantedGrade == "C-"){
+		var min = 70;
+		missing = min - totalGot;
+	}else if (wantedGrade == "D+"){
+		var min = 67;
+		missing = min - totalGot;
+	}else if (wantedGrade == "D"){
+		var min = 63;
+		missing = min - totalGot;
+	}else if (wantedGrade == "D-"){
+		var min = 60;
+		missing = min - totalGot;
+	}else if (wantedGrade == "F"){
+		var min = 0;
+		missing = min - totalGot;
+	}else {
+		output = "Unkown letter grade.";
+	}
+	
+	if (output == ""){
+		output = "You need " + missing + " more points to get the grade you desire.";
+	}
+
+	document.getElementById('result_score').innerHTML = output;
 }
